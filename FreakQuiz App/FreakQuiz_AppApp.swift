b247e20338d157b685ba7127f56f920d94e0bc0b
José Fumanal <jfumanal92@gmail.com>
2021-07-22 12:38:00 +0200

    Initial Commit

---
+++ FreakQuiz App/FreakQuiz_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  FreakQuiz_AppApp.swift
+//  FreakQuiz App
+//
+//  Created by Jose Fumanal Quintana on 22/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FreakQuiz_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
