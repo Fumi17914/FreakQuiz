0955de44e91dee40395e85d685850543de747838
José Fumanal <jfumanal92@gmail.com>
2021-07-28 18:57:24 +0200

    tutorial

--- FreakQuiz App/View/TutorialView.swift
+++ FreakQuiz App/View/TutorialView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct TutorialView: View {
+    
+    let viewModel = ChooseModeVM()
+    
     var body: some View {
         ZStack {
             Image("Background")
@@ -18,33 +21,228 @@ struct TutorialView: View {
                 TabView {
                     RoundedRectangle(cornerRadius: 25.0)
                         .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 250, alignment: .center)
+                        .foregroundColor(.white)
+                        .overlay(
+                            
+                            VStack {
+                                VStack {
+                                    Spacer()
+                                    
+                                    Image("textLogo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 290, height: 50, alignment: .center)
+                                    Image("logo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 120, height: 120, alignment: .center)
+
+                                    Spacer()
+
+                                    Text("Un juego de preguntas frikis")
+                                        .multilineTextAlignment(.leading)
+                                        .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                        .foregroundColor(.black)
+                                    Spacer()
+                                    Text("Si fallas, deberás realizar un prueba como penitencia")
+                                        .multilineTextAlignment(.leading)
+                                        .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                        .foregroundColor(.black)
+                                    Spacer()
+                                }.padding()
+                                Spacer(minLength: 50)
+                            }
+                        )
+                        .offset(y: -25)
+                    
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 250, alignment: .center)
+                        .foregroundColor(.orange)
+                        .overlay(
+                            VStack {
+                                VStack {
+                                    Spacer()
+                                    Image("addRed")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 100, height: 100, alignment: .center)
+                                    
+                                    Text("Botón para añadir jugadores en medio de la partida")
+                                        .multilineTextAlignment(.center)
+                                        .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/40))
+                                        .foregroundColor(.black)
+                                }
+                                Spacer(minLength: 50)
+                                VStack {
+                                    Image("scorePurple")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 100, height: 100, alignment: .center)
+                                    Text("Botón para mirar vuestras puntuaciones")
+                                        .multilineTextAlignment(.center)
+                                        .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/40))
+                                        .foregroundColor(.black)
+                                    Spacer()
+                                }
+                            }.padding()
+                        )
+                        .offset(y: -25)
+
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 250, alignment: .center)
                         .foregroundColor(.yellow)
                         .overlay(
-                            Text("")
+                            VStack {
+                                Spacer()
+                                Image("easyMode")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                Spacer()
+                                Text("Este es el modo más sencillo")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                Text("Os encontraréis preguntas de cultura general sobre el mundo friki")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                
+                                Spacer()
+                                NavigationLink(
+                                    destination: ChoosePlayerView().onAppear {self.viewModel.easyMode()},
+                                    label: {
+                                        Image("pressStart")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .padding()
+                                            
+                    
+                                            
+                                    })
+                                Spacer()
+                        
+                            }.padding()
                         )
                         .offset(y: -25)
                        
                     RoundedRectangle(cornerRadius: 25.0)
                         .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 250, alignment: .center)
-                        .foregroundColor(.red)
+                        .foregroundColor(.red).opacity(0.85)
                         .overlay(
-                            Text("")
+                            VStack {
+                                Spacer()
+                                Image("difficultMode")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                Spacer()
+                                
+                                Text("Bienvenid@s al modo difícil, un desafío")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                Text("Si crees que eres buen@, aquí podrás demostrarlo")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                NavigationLink(
+                                    destination: ChoosePlayerView().onAppear {self.viewModel.hardMode()},
+                                    label: {
+                                        Image("pressStart")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .padding()
+                                            
+                    
+                                            
+                                    })
+                                Spacer()
+                        
+                            }.padding()
+
                         )
                         .offset(y: -25)
                     RoundedRectangle(cornerRadius: 25.0)
                         .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 250, alignment: .center)
                         .foregroundColor(.blue)
                         .overlay(
-                            Text("")
+                            VStack {
+                                Spacer()
+                                Image("fastMode")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                Spacer()
+                                
+                                Text("Ahora os presentamos el modo rápido, con otro tipo de preguntas")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                Text("Antes sólo teníais que ser buen@s. Ahora también rápid@s")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                NavigationLink(
+                                    destination: ChoosePlayerView().onAppear {self.viewModel.fastMode()},
+                                    label: {
+                                        Image("pressStart")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .padding()
+                                            
+                    
+                                            
+                                    })
+                                Spacer()
+                        
+                            }.padding()
                         )
                         .offset(y: -25)
                     RoundedRectangle(cornerRadius: 25.0)
                         .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 250, alignment: .center)
                         .foregroundColor(.purple)
                         .overlay(
-                            Text("")
+                            VStack {
+                                Spacer()
+                                Image("hardcoreMode")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                Spacer()
+                                
+                                Text("Y por último, el modo hardcore, El tritura mentes friki")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                Text("Las preguntas son muy rebuscadas y... las respuestas también habrá que rebuscarlas...")
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.custom("PixelEmulator", size: UIScreen.main.bounds.height/33))
+                                    .foregroundColor(.black)
+                                Spacer()
+                                NavigationLink(
+                                    destination: ChoosePlayerView().onAppear {self.viewModel.thanosMode()},
+                                    label: {
+                                        Image("pressStart")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .padding()
+                                            
+                    
+                                            
+                                    })
+                                Spacer()
+                        
+                            }.padding()
                         )
                         .offset(y: -25)
+    
                 }.tabViewStyle(PageTabViewStyle())
                 .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode:.automatic))
             }
